a74243a784520001b7c086194984eca4c6bb0a84
iTrooz <hey@itrooz.fr>
2023-01-31 21:53:15 +0100

    put defualt insertion actions in class Inserter

--- packageviewer/inserters/dnf_inserter.py
+++ packageviewer/inserters/dnf_inserter.py
@@ -21,55 +21,6 @@ class DnfInserter(Inserter):
             CREATE TEMPORARY TABLE IF NOT EXISTS tmp_repo(repo_id INTEGER, name TEXT)
         ''')
 
-    
-    @timer.dec
-    def insert_distro(self, distro_name, distro_version):
-        cursor = self.conn.execute('''
-            INSERT INTO distro (name, version) VALUES (?, ?)
-        ''', (distro_name, distro_version))
-        self.conn.commit()
-        self.cache_distro_id = cursor.lastrowid
-
-    @timer.dec
-    def insert_repo_table(self):
-        self.conn.execute('''
-            INSERT INTO repo(distro_id, name)
-            SELECT DISTINCT ?, tmp_package.repo FROM tmp_package
-        ''', (self.cache_distro_id,))
-
-        self.conn.execute('''
-            INSERT INTO tmp_repo(repo_id, name)
-            SELECT DISTINCT repo.repo_id, repo.name FROM repo
-            JOIN distro ON distro.distro_id = ?
-        ''', (self.cache_distro_id,))
-        self.conn.commit()
-
-    @timer.dec
-    def insert_package_table(self):
-        self.conn.execute('''
-            INSERT INTO package (name, repo_id)
-            SELECT tmp_package.name, tmp_repo.repo_id FROM tmp_package
-            JOIN tmp_repo
-            ON tmp_repo.name = repo
-        ''')
-        self.conn.commit()
-
-    @timer.dec
-    def insert_dirname_table(self):
-        self.conn.execute('''
-            INSERT INTO dirname (dirname)
-            SELECT DISTINCT dirname FROM tmp_file
-        ''')
-        self.conn.commit()
-
-    @timer.dec
-    def insert_filename_table(self):
-        self.conn.execute('''
-            INSERT INTO filename (filename)
-            SELECT DISTINCT filename FROM tmp_file
-        ''')
-        self.conn.commit()
-
     @timer.dec
     def insert_file_table(self):
         self.conn.execute('''
@@ -83,17 +34,3 @@ class DnfInserter(Inserter):
             WHERE tmp_file.repo = tmp_repo.name
         ''')
         self.conn.commit()
-
-    def normalize(self, distro_name, distro_version):
-        self.create_tables()
-
-        self.insert_distro(distro_name, distro_version)
-
-        self.insert_repo_table()
-
-        self.insert_package_table()
-
-        self.insert_dirname_table()
-        self.insert_filename_table()
-
-        self.insert_file_table()
--- packageviewer/inserters/inserter.py
+++ packageviewer/inserters/inserter.py
@@ -9,7 +9,6 @@ class Inserter:
         
     @timer.dec
     def create_tables(self):
-
         self.conn.executescript('''
         CREATE TABLE IF NOT EXISTS distro(
             distro_id INTEGER PRIMARY KEY,
@@ -40,3 +39,86 @@ class Inserter:
             filename TEXT
         );
         ''')
+    
+    @timer.dec
+    def insert_distro(self, distro_name, distro_version):
+        cursor = self.conn.execute('''
+            INSERT INTO distro (name, version) VALUES (?, ?)
+        ''', (distro_name, distro_version))
+        self.conn.commit()
+        self.cache_distro_id = cursor.lastrowid
+
+    @timer.dec
+    def insert_repo_table(self):
+        self.conn.execute('''
+            INSERT INTO repo(distro_id, name)
+            SELECT DISTINCT ?, tmp_package.repo FROM tmp_package
+        ''', (self.cache_distro_id,))
+
+        self.conn.execute('''
+            INSERT INTO tmp_repo(repo_id, name)
+            SELECT DISTINCT repo.repo_id, repo.name FROM repo
+            JOIN distro ON distro.distro_id = ?
+        ''', (self.cache_distro_id,))
+        self.conn.commit()
+
+    @timer.dec
+    def insert_package_table(self):
+        self.conn.execute('''
+            INSERT INTO package (name, repo_id)
+            SELECT tmp_package.name, tmp_repo.repo_id FROM tmp_package
+            JOIN tmp_repo
+            ON tmp_repo.name = repo
+        ''')
+        self.conn.commit()
+
+    @timer.dec
+    def insert_dirname_table(self):
+        self.conn.execute('''
+            INSERT INTO dirname (dirname)
+            SELECT DISTINCT tmp_file.dirname FROM tmp_file
+            LEFT JOIN dirname ON dirname.dirname = tmp_file.dirname
+            WHERE dirname.dirname_id is null
+        ''')
+        self.conn.commit()
+
+    @timer.dec
+    def insert_filename_table(self):
+        self.conn.execute('''
+            INSERT INTO filename (filename)
+            SELECT DISTINCT tmp_file.filename FROM tmp_file
+            LEFT JOIN filename ON filename.filename = tmp_file.filename
+            WHERE filename.filename_id is null
+        ''')
+        self.conn.commit()
+
+    @timer.dec
+    def insert_file_table(self):
+        self.conn.execute('''
+            INSERT INTO file (package_id, dirname_id, filename_id)
+            SELECT package_id, dirname_id, filename_id FROM tmp_file
+            -- join package
+            JOIN package ON package.name = tmp_file.package
+            -- filter to keep our packages only
+            JOIN tmp_repo ON tmp_repo.repo_id = package.repo_id
+            JOIN distro ON tmp_repo.repo_id = package.repo_id
+            -- join dirname
+            JOIN dirname ON dirname.dirname = tmp_file.dirname
+            -- join filename
+            JOIN filename ON filename.filename = tmp_file.filename
+        ''')
+        self.conn.commit()
+
+    def normalize(self, distro_name, distro_version):
+        self.create_tables()
+
+        self.insert_distro(distro_name, distro_version)
+
+        self.insert_repo_table()
+
+        self.insert_package_table()
+
+        self.insert_dirname_table()
+        self.insert_filename_table()
+
+        self.insert_file_table()
