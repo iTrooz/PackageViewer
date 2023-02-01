b784417aab33bae0b91a098eca58aec75a9d41fb
iTrooz <hey@itrooz.fr>
2023-02-01 17:28:19 +0100

    add support for generators in SQLTable

--- packageviewer/sql_table.py
+++ packageviewer/sql_table.py
@@ -1,3 +1,5 @@
+import itertools
+
 class SQLTable:
 
     def __init__(self, conn, table_name: str, create_query: str) -> None:
@@ -6,9 +8,10 @@ class SQLTable:
         self.conn.execute(create_query)
 
     def add_rows(self, rows: list):
-        fields = ", ".join(rows[0].keys())
-        values = ", ".join("?"*len(rows[0]))
+        first_row = next(iter(rows))
+        fields = ", ".join(first_row.keys())
+        values = ", ".join("?"*len(first_row))
         sql = f"INSERT INTO {self.table_name} ({fields}) VALUES ({values})"
-        
-        insert_data = (list(row.values()) for row in rows)
+
+        insert_data = (list(row.values()) for row in itertools.chain((first_row,), rows))
         self.conn.executemany(sql, insert_data)
