beee85f014bc1724ab6b3512fcf45a329b923d50
iTrooz <hey@itrooz.fr>
2023-02-05 00:51:31 +0100

    add add_row() to SQLTable

--- packageviewer/sql_table.py
+++ packageviewer/sql_table.py
@@ -7,15 +7,24 @@ class SQLTable:
         self.table_name = table_name
         self.conn.execute(create_query)
 
-    def add_rows(self, rows: list):
+    def _get_sql_query_(self, keys):
+        fields = ", ".join(keys)
+        values = ", ".join("?"*len(keys))
+        return f"INSERT INTO {self.table_name} ({fields}) VALUES ({values})"
+
+
+    def add_row(self, row: dict):
+        sql = self._get_sql_query_(row.keys())
+        self.conn.execute(sql, list(row.values()))
+
+
+    def add_rows(self, rows): # 'rows' is iterable of dict
         first_row = next(iter(rows), None)
         
         if first_row == None: # empty iterator
             return
 
-        fields = ", ".join(first_row.keys())
-        values = ", ".join("?"*len(first_row))
-        sql = f"INSERT INTO {self.table_name} ({fields}) VALUES ({values})"
+        sql = self._get_sql_query_(first_row.keys())
 
         insert_data = (list(row.values()) for row in itertools.chain((first_row,), rows))
         self.conn.executemany(sql, insert_data)
