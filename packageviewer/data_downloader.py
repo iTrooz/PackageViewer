806ef2d799f6b39e9d36fd90aa78901091a758cb
iTrooz <hey@itrooz.fr>
2023-02-09 00:44:49 +0100

    download config stub

---
+++ packageviewer/data_downloader.py
@@ -0,0 +1,104 @@
+import yaml
+import os
+import itertools
+import asyncio
+import aiohttp
+
+def bytes_to_mib(bytes_n):
+    return round(bytes_n/(8*1024*1024), 2)
+
+class RepoData:
+    def __init__(self, archive_url, md) -> None:
+        self.archive_url = archive_url
+        self.md = md
+
+    def process_archive_url(self):
+        for key, value in self.md.items():
+            self.archive_url = self.archive_url.replace("$"+key, value)
+
+        if "$" in self.archive_url:
+            print("Warning: found unprocessed variable in archive url "+self.archive_url)
+
+    def __str__(self):
+        return f"RepoData[archive_url={self.archive_url}, md={self.md}]"
+
+    def __repr__(self):
+        return self.__str__()
+
+class DataDownloader:
+    def __init__(self, conf_filepath) -> None:
+        self.conf_filepath = conf_filepath
+
+    def _get_dist_repos(self, distro_name, dist_obj):
+        distro_archive_url = dist_obj.get("archive")
+
+        for version in dist_obj.get("versions", [{}]):
+            for repo_name, repo_obj in dist_obj.get("repos").items():
+                url_md = {}
+                if not repo_obj.get("version_agnostic"):
+                    url_md.update(version)
+                url_md["pm_type"] = dist_obj["pm_type"]
+                url_md["repo"] = repo_name
+                url_md["distro"] = distro_name
+
+                archive_url = repo_obj.get("archive") or distro_archive_url
+
+                archive = RepoData(archive_url, url_md)
+                archive.process_archive_url()
+                yield archive
+
+    def _get_files(self, repos):
+        for repo in repos:
+            url = None
+            match repo.md["pm_type"]:
+                case "apt":
+                    url = f'{repo.archive_url}/Contents-amd64.gz'
+                case "dnf":
+                    url = f'{repo.archive_url}/repodata/repomd.xml'
+                case "pacman":
+                    url = f'{repo.archive_url}/{repo.md["repo"]}.db'
+            
+            yield [url, os.path.join('archives',
+                    repo.md["distro"],
+                    repo.md.get("version_id") or "DEFVERSION",
+                    repo.md.get("repo") or "DEFREPO",
+                    os.path.basename(url)
+                )]
+
+    async def process(self):
+        repos = itertools.chain(*self._get_repos())
+        files = list(self._get_files(repos))
+        
+        print(f"Files to download: {len(files)}")
+        
+        client = aiohttp.ClientSession()
+        tasks = []
+        for url, file in files:
+            task = client.head(url, allow_redirects=True)
+            tasks.append(task)
+        
+        total = 0
+        results = await asyncio.gather(*tasks)
+        for result in results:
+            if result.ok:
+                size = int(result.headers["Content-Length"])
+                print(f"{result.url} : {bytes_to_mib(size)}MiB")
+
+                total += size
+            else:
+                print(f"Warning: request {result.url} returned HTTP code {result.status}")
+
+        mib_total = bytes_to_mib(total)
+        print(f'Total to download: {mib_total}MiB')
+
+        await client.close()
+
+        
+
+        
+        
+    def _get_repos(self):
+        f = yaml.safe_load(open(self.conf_filepath))
+        
+        for dist_name, obj in f.get("dists").items():
+            yield self._get_dist_repos(dist_name, obj)
